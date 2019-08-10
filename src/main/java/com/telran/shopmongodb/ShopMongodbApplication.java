eeca631c39224d822d50dd29a9bd27b1041001e4
vitalii.adler <adler.vitalii@gmail.com>
2019-08-10 14:05:07 +0300

    Initial commit

---
+++ src/main/java/com/telran/shopmongodb/ShopMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.telran.shopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMongodbApplication.class, args);
+    }
+
+}
